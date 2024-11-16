2aa15ef59d34ab902c7cd0b8bfb4ae64b392b99f
Josué Umba <tembojosue@gmail.com>
2024-11-16 02:53:58 +0100

    Responsive du header

--- header.php
+++ header.php
@@ -5,33 +5,33 @@ if (session_status() == PHP_SESSION_NONE) {
 
 ?>
 
-<header class="d-flex">
+<header class="d-flex" id="header">
     <?php if(isset($_SESSION['logged_student'])): ?>
-        <p style="font-size: 20px;"><a href="home.php">USCITECH VOTE</a></p>
+        <p><a href="home.php">USCITECH VOTE</a></p>
     <?php else :?>
-        <p style="font-size: 20px;"><a href="index.php">USCITECH VOTE</a></p>
+        <p><a href="index.php">USCITECH VOTE</a></p>
     <?php endif; ?>
 
     <?php if(isset($_SESSION['logged_student'])): ?>
-        <p style="font-weight:600; font-size:1.5em; border:1px solid white; border-radius: 50px; padding:5px 10px">
+        <p style="font-weight:600; border:1px solid white; border-radius: 50px; padding:5px 10px">
             <?php echo $_SESSION['logged_student']['prenom'] . " " . $_SESSION['logged_student']['nom'] ?>
         </p>
     <?php endif; ?>
 
     <nav>
         <?php if(isset($_SESSION['logged_student'])): ?>
-            <a href="home.php">Home</a>
+            <a href="home.php" id="hidden">Home</a>
             <?php if($_SESSION['logged_student']['promotion'] == "GOLD") : ?>
                 <a href="results_form.php">Results</a>
             <?php else : ?>
-                <a href="vote.php">Vote</a>
+                <a href="vote.php" id="hidden">Vote</a>
             <?php endif; ?>
-            <a href="about.php">About</a>
+            <a href="about.php" id="hidden">About</a>
             <a href="logout.php">LogOut</a>
         <?php else :?>
-            <a href="index.php">Home</a>
-            <a href="login.php">Login</a>
-            <a href="about.php">About</a>
+            <a href="index.php" id="hidden">Home</a>
+            <a href="login.php" id="hidden">Login</a>
+            <a href="about.php" id="hidden">About</a>
         <?php endif; ?>
     </nav>
 </header>
